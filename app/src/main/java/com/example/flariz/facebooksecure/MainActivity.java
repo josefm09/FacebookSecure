2f28b012c370c1ce326f5e9ac13e8d2f5cfab826
josefm09 <josecarlosfloresmoran@hotmail.com>
2018-03-27 18:09:00 -0700

    Se hicieron mejoras en la app

--- app/src/main/java/com/example/flariz/facebooksecure/MainActivity.java
+++ app/src/main/java/com/example/flariz/facebooksecure/MainActivity.java
@@ -2,16 +2,40 @@ package com.example.flariz.facebooksecure;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  private WebView myWebView;
 
-        WebView myWebView = findViewById(R.id.webview);
-        myWebView.loadUrl("https://www.facebook.com");
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+    //myWebView = findViewById(R.id.webview);
+    myWebView = new WebView(this);
+    myWebView.loadUrl("https://www.facebook.com");
+    myWebView.setWebViewClient(new WebViewClient() {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+      }
+    });
+
+    this.setContentView(myWebView);
+  }
+
+  @Override
+  public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+    if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+      myWebView.goBack();
+      return true;
     }
+    return super.onKeyDown(keyCode, event);
+  }
 }
